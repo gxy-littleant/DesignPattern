2b232d7d5d48d6672f769d2a5c892fe23d45b8de
gxy-littleant <634016585@qq.com>
2024-05-04 18:54:38 +0800

    question settled

--- adaptor/adaptor.cpp
+++ adaptor/adaptor.cpp
@@ -5,7 +5,22 @@
 
 using namespace std;
 
-// 有问题：为什么在第二个ClientCode函数中调用的不是 adaptee ？ 而是默认的target？
+/**
+    有问题：为什么在第二个ClientCode函数中调用的不是 adaptee ？ 而是默认的target？
+    原因：在 Adaptor 类中，Request（）函数没有写成const，导致Request（）成为新的函数，而不是重写基类的方法。 
+    愚蠢的错误，太折麽了！  
+    在 ClientCode函数中，参数类型若为 unique_ptr<Target> target， 则不能传进 unique_ptr<Adaptor> adaptor
+    unique_ptr不能进行隐式的类型    
+
+    问：unique_ptr可以从父类转换到子类吗？
+    答：不可以，std::unique_ptr不支持从父类指针到子类指针的隐式转换。这是因为在C++中，智能指针的目的是管理动态分配的内存，并确保在合适的时候释放它。
+    如果允许将父类指针转换为子类指针，则可能导致在释放时出现未定义的行为，因为父类指针可能指向的不是由子类分配的内存。
+    如果确实需要将父类指针转换为子类指针，可以使用 std::dynamic_pointer_cast 或者 std::static_pointer_cast，但是这些转换都需要明确的类型转换，而不是隐式转换。
+
+    问：share_ptr可以从父类转换到子类吗？
+    答：std::shared_ptr支持从父类指针转换为子类指针。这是因为std::shared_ptr使用引用计数来管理内存，允许多个指针共享同一块内存。
+    因此，即使从父类指针转换为子类指针，也不会导致内存管理问题。
+**/
 
 class Target {
     public:
@@ -30,7 +45,7 @@ class Adaptor : public Target {
 
     public:
         Adaptor(unique_ptr<Adaptee> ada) : adaptee_(std::move(ada)) {}
-        string Request() {
+        string Request() const override {
             string To_reverse_str = adaptee_->SpecificRequest();
             std::reverse(To_reverse_str.begin(),To_reverse_str.end());
             cout<< "To_reverse_str: " << To_reverse_str << endl;
@@ -38,25 +53,38 @@ class Adaptor : public Target {
         }
 };
 
-void ClientCode(unique_ptr<Target> const & target) {
+void ClientCode(const Target* target) {
     cout<< target->Request();
 };
 
+
+// void ClientCode(unique_ptr<Target> & target) {
+//     cout<< target->Request();
+// };
+
+
 int main() {
-    // Target * target = new Target();
     unique_ptr<Target> target = make_unique<Target>();
-    ClientCode(target);
+    ClientCode(target.get());
+    // ClientCode(target);
+
     std::cout << "\n\n";
 
     unique_ptr<Adaptee> adaptee = make_unique<Adaptee>();
+    // Adaptee* adaptee = new Adaptee();
+
     std::cout << "Client: The Adaptee class has a weird interface. See, I don't understand it:\n";
-    std::cout << "Adaptee: " << adaptee->SpecificRequest();
+    std::cout << "Adaptee: " << adaptee -> SpecificRequest();
     std::cout << "\n\n";
     std::cout << "Client: But I can work with it via the Adapter:\n";
 
-    unique_ptr<Target> adaptor = make_unique<Adaptor>(std::move(adaptee));
-    ClientCode(adaptor);
+    unique_ptr<Adaptor> adaptor = make_unique<Adaptor>(std::move(adaptee));
+    ClientCode(adaptor.get());
+    // ClientCode(adaptor);
+
     std::cout << "\n";
+
+    // delete adaptee;
 
     return 0;
 }
